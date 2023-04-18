2da143c8b83ced18851abd5fe6bacddd8c6b125d
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-04-18 22:37:04 +0700

    video 5 state

--- src/example/MyComponent.js
+++ src/example/MyComponent.js
@@ -1,8 +1,33 @@
 import React from "react";
 class MyComponent extends React.Component {
+
+    // key: value
+    state = {
+        name: "Lê Huỳnh Thanh Ngân",
+        channel: "Hoi Dan IT"
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    /*Comment
+    JSX => return block
+    fragment
+    state: dữ liệu có thể thay đổi, ngay lập tức cập nhật dữ liệu ngay lập tức không cần refersh lại
+    */
     render() {
         return (
-            <div>Hello my component Thanh Ngan</div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+                    My name is {this.state.name}
+                </div>
+                <div className="second">
+                    Youtube chanel : {this.state.channel}
+                </div>
+            </>
         )
     }
 }
