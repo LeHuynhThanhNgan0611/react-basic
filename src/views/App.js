133121912d989fa620b7f0aa1a1a9119236662d7
LeHuynhThanhNgan0611 <114009954+LeHuynhThanhNgan0611@users.noreply.github.com>
2023-06-03 23:42:18 +0700

    TODO APP

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from '../example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 /** 2 component: class component / function component (function, arrow)
  * 
  * JSX: function or hàm của js
@@ -13,10 +16,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world with React.js (Le Huynh Thanh Ngan)
+          Simple TODO Apps with React.js (Le Huynh Thanh Ngan &amp; Hoi Dan IT)
         </p>
-        <MyComponent></MyComponent>
+        {/* <MyComponent></MyComponent> */}
+        <ListTodo></ListTodo>
       </header>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+        {/* Same as */}
+      <ToastContainer />
     </div>
   );
 }
